5a3e253eeecb0244e09484df59f751b1cec139af
Huiimin5 <18200279735@163.com>
2022-12-16 21:35:58 +0800

    First stage

--- code/networks/__init__.py
+++
@@ -1,6 +0,0 @@
-from .vnet import VNet
-from .stochastic_vnet import StochasticVNet
-import sys, os
-sys.path.append(os.path.join(os.getcwd(), 'networks'))
-import losses
-__all__ = ["VNet", "StochasticVNet", "losses"]
